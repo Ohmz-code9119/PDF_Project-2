329017360fba417663babdfc5f203566c6bc0c53
omarm <omarmourad9119@gmail.com>
2023-08-27 01:43:15 -0400

    Day 24: Add lines to PDF

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)  # the order of the parameters is red, green, blue
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(x1=10, y1=21, x2=200, y2=21)
+    for y in range(20, 296, 10):
+        pdf.line(10, y, 200, y)
 
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=12)
@@ -21,6 +22,9 @@ for index, row in df.iterrows():
 
     for pages in range(row["Pages"] - 1):
         pdf.add_page()
+
+        for y in range(20, 296, 10):
+            pdf.line(10, y, 200, y)
 
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=12)
