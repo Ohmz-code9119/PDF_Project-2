b8e40fefb3086a4e768150a746ba2a1a02e3d28e
omarm <omarmourad9119@gmail.com>
2023-08-27 00:13:55 -0400

    Day 24: Add more pages for ach topic using for loop with range functions

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import pandas
 from fpdf import FPDF
 import pandas as pd
 
@@ -8,9 +7,13 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 for index, row in df.iterrows():
     pdf.add_page()
+
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)  # the order of the parameters is red, green, blue
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(x1=10, y1=21, x2=200, y2=21)
+
+    for pages in range(row["Pages"] - 1):
+        pdf.add_page()
 
 pdf.output("output.pdf")
